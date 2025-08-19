3cda583f17b8a07241bc36781727bc721ce74eda
kickhead13 <ana.alexandru.gabriel@proton.me>
2025-08-20 00:29:48 +0300

    fixed hook check

--- api/src/main.rs
+++ api/src/main.rs
@@ -9,9 +9,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
+    // connection to scylla-server
+    let session = actix_web::web::Data::new(security::structures::ScyllaSession {
         lock: std::sync::Mutex::new(db::prelude::new_scylla_session("onlinedi.vision:9042").await.expect(""))
     });
-
+    
     // setting up the API server
     let _ = actix_web::HttpServer::new(move || {
         actix_web::App::new()
