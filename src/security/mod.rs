793f4c4ee8a17462059465a6808d89678b512e14
kickhead13 <ana.alexandru.gabriel@proton.me>
2025-10-11 14:13:34 +0300

    core(ODIVE-1): fixed one typo/bug & added unit tests for new functions

--- src/security/mod.rs
+++ src/security/mod.rs
@@ -4,6 +4,26 @@ use rand::prelude::*;
 pub mod structures;
 pub mod aes;
 
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_token_armor() {
+        assert_eq!("6249cbd6de7d01973b4a73eacc503c275ec9a92b49306b5a713e998ce2104182", armor_token("token12345678901234567890".to_string()));
+    }
+
+    #[test]
+    fn test_sha256() {
+        assert_eq!("2bb80d537b1da3e38bd30361aa855686bde0eacd7162fef6a25fe97bf527a25b", sha256("secret".to_string()));
+    }
+
+    #[test]
+    fn test_hashers(){
+        let pre_hash_string: String = "pre_hash".to_string();
+        assert_ne!(sha256(pre_hash_string.clone()), sha512(pre_hash_string));
+    }
+}
+
 pub fn sha256(secret: String) -> String {
     let mut hasher = sha2::Sha256::new();
     hasher.update(secret.into_bytes());
@@ -11,7 +31,7 @@ pub fn sha256(secret: String) -> String {
 }
 
 pub fn sha512(secret: String) -> String {   
-    let mut hasher = sha2::Sha256::new();
+    let mut hasher = sha2::Sha512::new();
     hasher.update(secret.into_bytes());
     format!("{:x}", hasher.finalize())
 }
