36496c00a77fd25fb122cadffd4a5f16c634c7e5
kickhead13 <ana.alexandru.gabriel@proton.me>
2025-11-03 23:27:52 +0200

    core: [ODIVE-3] better encryption for server messages
    This change introduces a better method of encrypting user messages.
    By using a random (message-specific) salt we can encrypt the message
    then encrypt the salt itself using the API key and then save both
    (encrypted message and encrypted salt) in the DB. From there, when
    we need to retrieve messages we can decrypt messages one by one.
    
    Note that the launch-scylla-locally.sh changes are already done in
    the DB. The changes are necesarry for the migration to the new
    encryption method:
    - added new column *encrypted* (null on all entries before this
    commit)
    - added new column *salt*

--- src/security/aes.rs
+++ src/security/aes.rs
@@ -1,4 +1,4 @@
-use aes::cipher::{block_padding::Pkcs7, BlockDecryptMut, BlockEncryptMut, KeyIvInit};
+use aes::cipher::{BlockDecryptMut, BlockEncryptMut, KeyIvInit, block_padding::Pkcs7};
 use hex;
 
 type Aes128CbcEnc = cbc::Encryptor<aes::Aes128>;
@@ -8,14 +8,12 @@ use crate::env;
 
 #[inline(always)]
 pub fn encrypt_with_key(plaintext: &str, key: &str) -> String {
-    // let key_bytes = hex::encode(key.to_string()).unwrap();
-    let iv_bytes = env::get_env_var(env::statics::OD_AES_IV);  
+    let iv_bytes = env::get_env_var(env::statics::OD_AES_IV);
     let cipher = Aes128CbcEnc::new_from_slices(key.as_bytes(), iv_bytes.as_bytes());
     let mut buffer = plaintext.as_bytes().to_vec();
     let pos = plaintext.len();
     let block_size = 16;
     let padding_needed = block_size - (pos % block_size);
-    // buffer.resize(pos + 16 - (pos % 16), 0);
     buffer.resize(pos + padding_needed, 0);
     let ciphertext = cipher
         .expect("")
@@ -26,26 +24,12 @@ pub fn encrypt_with_key(plaintext: &str, key: &str) -> String {
 
 #[inline(always)]
 pub fn encrypt(plaintext: &str) -> String {
-    // let key_bytes = hex::decode(env::get_env_var(env::statics::OD_AES_KEY)).unwrap();
-    // let iv_bytes = hex::decode(env::get_env_var(env::statics::OD_AES_IV)).unwrap();  
-    // let cipher = Aes128CbcEnc::new_from_slices(&key_bytes, &iv_bytes);
-    // let mut buffer = plaintext.as_bytes().to_vec();
-    // let pos = plaintext.len();
-    // let block_size = 16;
-    // let padding_needed = block_size - (pos % block_size);
-    // // buffer.resize(pos + 16 - (pos % 16), 0);
-    // buffer.resize(pos + padding_needed, 0);
-    // let ciphertext = cipher
-    //     .expect("")
-    //     .encrypt_padded_mut::<Pkcs7>(&mut buffer, pos)
-    //     .unwrap();
-    // hex::encode(ciphertext)
     encrypt_with_key(plaintext, &env::get_env_var(env::statics::OD_AES_KEY))
 }
 
 #[inline(always)]
-pub fn decrypt(ciphertext: &str) -> String {
-    let key_bytes = env::get_env_var(env::statics::OD_AES_KEY);
+pub fn decrypt_with_key(ciphertext: &str, key: &str) -> String {
+    let key_bytes = key.to_string();
     let iv_bytes = env::get_env_var(env::statics::OD_AES_IV);
     let mut ciphertext_bytes = hex::decode(ciphertext).unwrap();
     let cipher = Aes128CbcDec::new_from_slices(key_bytes.as_bytes(), iv_bytes.as_bytes());
@@ -54,4 +38,9 @@ pub fn decrypt(ciphertext: &str) -> String {
         .decrypt_padded_mut::<Pkcs7>(&mut ciphertext_bytes)
         .unwrap();
     String::from_utf8(plaintext.to_vec()).expect("")
+}
+
+#[inline(always)]
+pub fn decrypt(ciphertext: &str) -> String {
+    decrypt_with_key(ciphertext, &env::get_env_var(env::statics::OD_AES_KEY))
 }
--- src/security/mod.rs
+++ src/security/mod.rs
@@ -1,25 +1,33 @@
-use sha2::Digest;
 use rand::prelude::*;
+use sha2::Digest;
 
-pub mod structures;
 pub mod aes;
+pub mod messages;
+pub mod structures;
 
 mod tests {
-    #[allow(unused_imports)] // This is for `use super::*;` -- for some reason it doesn't like it without allowing used imports
+    #[allow(unused_imports)]
+    // This is for `use super::*;` -- for some reason it doesn't like it without allowing used imports
     use super::*;
-    
+
     #[test]
     fn test_token_armor() {
-        assert_eq!("6249cbd6de7d01973b4a73eacc503c275ec9a92b49306b5a713e998ce2104182", armor_token("token12345678901234567890".to_string()));
+        assert_eq!(
+            "6249cbd6de7d01973b4a73eacc503c275ec9a92b49306b5a713e998ce2104182",
+            armor_token("token12345678901234567890".to_string())
+        );
     }
 
     #[test]
     fn test_sha256() {
-        assert_eq!("2bb80d537b1da3e38bd30361aa855686bde0eacd7162fef6a25fe97bf527a25b", sha256("secret".to_string()));
+        assert_eq!(
+            "2bb80d537b1da3e38bd30361aa855686bde0eacd7162fef6a25fe97bf527a25b",
+            sha256("secret".to_string())
+        );
     }
 
     #[test]
-    fn test_hashers(){
+    fn test_hashers() {
         let pre_hash_string: String = "pre_hash".to_string();
         assert_ne!(sha256(pre_hash_string.clone()), sha512(pre_hash_string));
     }
@@ -31,7 +39,7 @@ pub fn sha256(secret: String) -> String {
     format!("{:x}", hasher.finalize())
 }
 
-pub fn sha512(secret: String) -> String {   
+pub fn sha512(secret: String) -> String {
     let mut hasher = sha2::Sha512::new();
     hasher.update(secret.into_bytes());
     format!("{:x}", hasher.finalize())
@@ -40,42 +48,28 @@ pub fn sha512(secret: String) -> String {
 pub fn token() -> String {
     let salt = uuid::Uuid::now_v7().to_string();
     println!("t{}", salt);
-    
-    let mut hasher = sha2::Sha256::new();
-    
-    hasher.update(
-        format!(
-            "{}", 
-            salt, 
-        ).to_string().into_bytes()
-    );
 
-    format!(
-        "{:x}", 
-        hasher.finalize()
-    )
+    let mut hasher = sha2::Sha256::new();
+
+    hasher.update(format!("{}", salt,).to_string().into_bytes());
+
+    format!("{:x}", hasher.finalize())
 }
 
 pub fn armor_token(plain_token: String) -> String {
-    sha256(
-        aes::encrypt(
-            &aes::encrypt_with_key(
-                &plain_token,
-                &plain_token[..16]
-            )
-        )
-    )
+    sha256(aes::encrypt(&aes::encrypt_with_key(
+        &plain_token,
+        &plain_token[..16],
+    )))
 }
 
 pub fn sid() -> String {
-    format!(
-        "{}{}",
-        token(),
-        rand::rng().random::<u64>()
-    )
+    format!("{}{}", token(), rand::rng().random::<u64>())
 }
 
 pub fn salt() -> String {
     let mut rng = rand::rng();
-    (0..16).map(|_| rng.random_range::<u8, _>(33..127) as char).collect()
+    (0..16)
+        .map(|_| rng.random_range::<u8, _>(33..127) as char)
+        .collect()
 }
