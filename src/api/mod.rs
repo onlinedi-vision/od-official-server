f32f5db2248d00902b12ae314dfb3c7663567930
Alex_Duna <daniel.alex17@yahoo.com>
2025-12-06 19:47:56 +0200

    Moved all macros from mod.rs in newly created prelude.rs

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -3,41 +3,8 @@
 /* !TODO:
  *  -- check out passing secrets with GET requests (to replace weird POST request implementation)
  * */
-macro_rules! scylla_session {
-    ($session:ident) => {
-        match $session.lock.lock() {
-            Ok(guard) => guard,
-            Err(_) => {
-                return actix_web::HttpResponse::InternalServerError()
-                    .body("Internal error: scylla session lock poisoned.");
-            }
-        }
-    };
-}
-
-macro_rules! cache {
-    ($shared_cache:ident) => {
-        match $shared_cache.lock.lock() {
-            Ok(guard) => guard,
-            Err(_) => {
-                return actix_web::HttpResponse::InternalServerError()
-                    .body("Internal error: cache lock poisoned.");
-            }
-        }
-    };
-}
-
-macro_rules! param {
-    ($http:expr, $name:expr) => {
-        match $http.match_info().get($name) {
-            Some(param) => param.to_string(),
-            None => {
-                return actix_web::HttpResponse::BadRequest()
-                    .body(format!("missing `{}` parameter", $name));
-            }
-        }
-    };
-}
+#[macro_use]
+pub mod prelude;
 
 pub mod channel;
 pub mod friends;
