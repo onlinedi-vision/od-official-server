b9a6c0fb360462a2a6ecda0d27cba2fb543c5a93
Ana Alexandru-Gabriel <alexandru.ana@nokia.com>
2026-09-08 10:01:17 +0300

    /get_user_servers should return empty array if user is in no server
    This fix should prevent current client failures here... The api
    server would respond with an HTML error for perfectly normal
    behaviour (a user can in fact be in no server)

---
+++ test-env-compose/e2e/test_user.py
@@ -0,0 +1,10 @@
+from base import BaseAPITestCase
+
+class UserTest(BaseAPITestCase):
+    def check_user_servers_list(self):
+        response = self._post("/get_user_servers", {
+            "username": self.username,
+            "token": self.token
+        })
+        s_list = response.get("s_list")
+        self.assertEqual(s_list, [])
